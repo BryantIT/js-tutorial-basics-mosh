74a60daefae15ee5f86db0fe5a3f3d6a4ffc5888
Bryant Richards <dragonseyeimages@gmail.com>
2020-08-05 09:49:11 -0500

    Dynamic Types

--- index.js
+++ index.js
@@ -15,8 +15,15 @@
 // interestRate = 1 // Will cause an error
 
 // Primitives
-let name = 'Bryant' // String Literal
-let age = 30 // Number Literal
-let isApproved = true // Boolean Literal
-let firstName = undefined // Not common
-let selectedColor = null // Clears the value of a variable 
+// let name = 'Bryant' // String Literal
+// let age = 30 // Number Literal
+// let isApproved = true // Boolean Literal
+// let firstName = undefined // Not common
+// let selectedColor = null // Clears the value of a variable
+
+// Dynamic
+let name = 'Bryant'
+let age = 38
+let isApproved = true
+let firstName = undefined
+let selectedColor = null
