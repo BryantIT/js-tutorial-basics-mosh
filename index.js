33d61d2077bd8e84b1af841fe421b16f682e7eb9
Bryant Richards <dragonseyeimages@gmail.com>
2020-08-05 09:31:51 -0500

    Variables

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-// Going back to basics
-console.log('Hello World')
+let name = 'Bryant'
+
+console.log(name)
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number
+// Cannot contain a space or hyphen
+// Are case-sensitive
+
+let firstName = 'Bryant'
+let lastName = 'Richards'
